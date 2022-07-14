83f11cc1b970dbb4fc08ee80558ad3dda4b8402a
Jawid Jahish <jawidjahish@Jawids-MBP.attlocal.net>
2022-07-13 19:38:53 -0700

    update

---
+++ src/ExamplesForGirls/SwitchCases.java
@@ -0,0 +1,21 @@
+package ExamplesForGirls;
+
+public class SwitchCases {
+	
+	public static void main(String[] args) {
+		
+		
+		String str = "test";
+		
+		switch(str) {
+		case "test":
+			System.out.println("this is right");
+			break;
+		case "test1":
+			System.out.println("this is wrong");
+			default:
+				System.out.println("default");
+		}
+	}
+
+}
